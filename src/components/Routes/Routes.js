3374fddca58fa832a0a9b15e8a0856625d13f921
Progna_Labony_Roy <labony.progna@gmail.com>
2022-11-07 23:48:22 +0530

    header and route added

---
+++ src/components/Routes/Routes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Home from '../Home/Home';
+import Main from '../Layout/Main';
+
+const { createBrowserRouter } = require("react-router-dom");
+
+const router= createBrowserRouter([
+    {
+      path:'/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element:<Home></Home>
+        },
+      ]
+    }
+  ]);
+
+  export default router;
