cf1ffa2809078695111d2a5e1cac044418610adc
Progna_Labony_Roy <labony.progna@gmail.com>
2022-11-08 08:20:11 +0530

    auth context added

---
+++ src/components/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCd59RpEF6BzXyON2oA2X-2TXfTN8L5XZ0",
+  authDomain: "service-review-7a244.firebaseapp.com",
+  projectId: "service-review-7a244",
+  storageBucket: "service-review-7a244.appspot.com",
+  messagingSenderId: "1074573339301",
+  appId: "1:1074573339301:web:0f7a34a83ee1b3ddaebb4c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
