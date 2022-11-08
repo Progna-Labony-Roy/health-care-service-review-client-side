4d734b74e4ef65618448bd328678428715c4f7f3
Progna_Labony_Roy <labony.progna@gmail.com>
2022-11-09 00:21:39 +0530

    favicon added

--- src/components/Routes/PrivateRoute.js
+++ src/components/Routes/PrivateRoute.js
@@ -7,7 +7,13 @@ const PrivateRoute = ({children}) => {
     const location =useLocation();
 
     if(loading){
-        return <></>
+        return (
+            <div class="flex justify-center items-center">
+  <div class="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
+    <span class="visually-hidden">Loading...</span>
+  </div>
+</div>
+        )
     }
 
     if(user){
